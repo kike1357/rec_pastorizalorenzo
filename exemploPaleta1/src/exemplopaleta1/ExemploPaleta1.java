ea7f530e612b79a2aca22212c8642dfc83eb6423
epastorizalorenzo <epastorizalorenzo@atanasoff26.danielcastelao.org>
2016-05-11 11:09:48 +0200

    commit gitHub

---
+++ exemploPaleta1/src/exemplopaleta1/ExemploPaleta1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplopaleta1;
+
+/**
+ *
+ * @author epastorizalorenzo
+ */
+public class ExemploPaleta1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaBoton obx = new VentanaBoton();
+        obx.setVisible(true);
+    }
+    
+}
